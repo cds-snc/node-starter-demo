d6f54c4954c61d2ca759cbf99671b9e8b3fced22
Tim Arney <tim@line37.com>
2019-09-12 11:26:07 -0400

    make the field not required

--- routes/personal/schema.js
+++ routes/personal/schema.js
@@ -1,10 +1,5 @@
 const Schema = {
-  fullname: {
-    isLength: {
-      errorMessage: 'errors.fullname.length',
-      options: { min: 3, max: 200 },
-    },
-  },
+  fullname: {},
 }
 
 module.exports = {
